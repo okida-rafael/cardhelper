e494525344348da62558bf59c5a2e4c5b3332967
Rafael <okida.rafael@gmail.com>
2023-10-06 21:10:48 -0300

    Fix footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { CssBaseline } from '@mui/material';
-import Footer from "./components/Footer";
+
 
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
         <Route path="/" element={<Menu/>}></Route>
         <Route path="/game" element={<Game />}></Route>
       </Routes>
-      <Footer />
+
     </>
   );
 }
